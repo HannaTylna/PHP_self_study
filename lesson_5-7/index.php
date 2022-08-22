e9c32035121aa5a5e539690f026901bbeb3cbf51
Hanna <hanna.tylna@gmail.com>
2022-08-22 15:31:11 +0200

    lesson 5-7

---
+++ lesson_5-7/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Variables</title>
+</head>
+<body>
+    <?php
+        $num_1 = 30;
+        $num_2 = 50;
+        $sum = $num_1 + $num_2;
+        echo "<br>";
+        echo "Hello world \"$sum\" \r\n";
+        echo "<br>";
+        echo $sum;
+    ?>
+    
+</body>
+</html>
